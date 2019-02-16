a55b56326e762e75a40493384261e1c6dcd131d3
Nabhoneel Majumdar <nabhoneel.95@gmail.com>
2019-02-17 00:47:07 +0530

    gallery added, sans images, still okay

---
+++ src/components/top-bar.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import Img from "gatsby-image";
+import { Link, StaticQuery } from "gatsby";
+
+import "./top-bar.scss";
+
+class TopBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false
+    };
+
+    this.location = "/";
+  }
+
+  componentDidMount() {
+    this.location = window.location.pathname;
+    this.setState({
+      loaded: true
+    });
+  }
+
+  renderBar() {
+    if (this.location) {
+      if (this.location !== "/") {
+        return (
+          <div className="top-bar">
+            <Link to="/">
+              <div className="logo-container">
+                <span className="logo-text">Reflexons 2019</span>
+                <StaticQuery
+                  query={graphql`
+                    query smallLogo {
+                      placeholderImage: file(relativePath: { eq: "logo.png" }) {
+                        childImageSharp {
+                          fluid {
+                            ...GatsbyImageSharpFluid
+                          }
+                        }
+                      }
+                    }
+                  `}
+                  render={data => (
+                    <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+                  )}
+                />
+              </div>
+            </Link>
+            <div className="social-links" />
+          </div>
+        );
+      }
+    }
+  }
+
+  render() {
+    return <>{this.state.loaded ? this.renderBar() : null}</>;
+  }
+}
+
+export default TopBar;
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,13 +1,101 @@
-import React from 'react';
+import React, { Component } from "react";
+import Img from "gatsby-image";
+import Slider from "react-slick";
 
-import Layout from '../components/layout';
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import SliderArrow from "../components/slider-arrows";
 
-const Gallery = () => (
-	<Layout>
-	<div className="gallery">
-	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur itaque quae animi voluptate esse enim praesentium consectetur illum excepturi nihil, tenetur quia tempora qui delectus quibusdam minus quos asperiores, minima magni consequatur natus, velit voluptas? Eum hic sint velit soluta odit ea nihil ducimus sunt. Ut soluta sit necessitatibus tempore nulla expedita eveniet accusantium optio repellendus recusandae ipsam ullam est quaerat corporis id, fugiat dolorum, molestiae aut mollitia quidem cumque iusto eum rerum pariatur? Ducimus temporibus exercitationem, reprehenderit molestiae rem repellat ipsam ex, dolorum ratione odit maxime facere aut iste soluta, aperiam sunt necessitatibus atque explicabo quaerat tenetur cumque inventore.
-	</div>
-	</Layout>
-);
+import "./gallery.scss";
+
+import { graphql } from "gatsby";
+
+class Gallery extends Component {
+  componentDidMount() {
+    const data = this.props.data;
+
+    data.allFile.edges.forEach(({ node }) => {
+      const url = "url('" + node.childImageSharp.fluid.src + "')";
+
+      document.getElementById(
+        `${node.id}`
+      ).firstChild.firstChild.style.backgroundImage = url;
+
+      document.getElementById(`${node.id}`).firstChild.firstChild.style.filter =
+        "blur(20px) brightness(50%)";
+    });
+  }
+
+  render() {
+    const data = this.props.data;
+
+    const settings = {
+      accessibility: false,
+      arrows: true,
+      adaptiveHeight: true,
+      dots: false,
+      infinite: true,
+      autoplay: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: <SliderArrow to="prev" icon="fas fa-angle-left" />,
+      nextArrow: <SliderArrow to="next" icon="fas fa-angle-right" />
+    };
+
+    return (
+      <>
+        <SEO
+          title="Gallery"
+          keywords={[
+            "reflexons",
+            "calcutta university",
+            "coding competition",
+            "games",
+						"counter strike",
+						"gallery"
+          ]}
+        />
+        <Layout>
+          <div className="Gallery">
+            <Slider {...settings}>
+              {data.allFile.edges.map(({ node }) => {
+                return (
+                  <div key={node.id} id={node.id}>
+                    <Img
+                      fluid={node.childImageSharp.fluid}
+                      style={{ background: `black` }}
+                    />
+                  </div>
+                );
+              })}
+            </Slider>
+          </div>
+        </Layout>
+      </>
+    );
+  }
+}
 
 export default Gallery;
+
+export const GalleryImages = graphql`
+  query {
+    allFile(
+      filter: { extension: { eq: "jpg" }, relativeDirectory: { eq: "gallery" } }
+    ) {
+      edges {
+        node {
+          id
+          name
+          relativeDirectory
+          childImageSharp {
+            fluid(maxWidth: 1280) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
